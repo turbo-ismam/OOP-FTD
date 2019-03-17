068d2568fdd2ffd1365ef9beb8034fede1a923c0
Hu Marcoagit <marco.hu2@studio.unibo.it>
2019-03-18 00:19:43 +0100

    workinprogress

--- src/Main/FantasyTD.java
+++ src/Main/FantasyTD.java
@@ -1,56 +1,12 @@
 package Main;
 
+import View.MainMenu.MainMenu;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
-
-public class FantasyTD extends Application implements EventHandler<ActionEvent>{
+public class FantasyTD{
 	
-	public static void main(String[] args) {
-		launch(args);
+	public static void main(String[] args){
+		Application.launch(MainMenu.class);
+		System.out.println("printline di debug");
 	}
-	Button buttonStart;
-	Button buttonExit;
-	
-	@Override
-	public void start(Stage PrimaryStage) throws Exception {
-			PrimaryStage.setTitle("FantasyTD 1.0");
-			
-			buttonStart = new Button();
-			buttonStart.setText("Start game");
-			buttonStart.setOnAction(this);
-			
-			buttonExit = new Button();
-			buttonExit.setText("Exit");
-			buttonExit.setOnAction(this);
-			
-			
-			 VBox layout = new VBox();
-			    layout.setPadding(new Insets(10));
-			    layout.setSpacing(8);
-			    layout.setAlignment(Pos.CENTER);
-			layout.getChildren().add(buttonStart);
-			layout.getChildren().add(buttonExit);
-			
-			
-			Scene scene = new Scene(layout, 150, 150);
-			PrimaryStage.setScene(scene);
-			PrimaryStage.show();
-		}
-	
-	@Override
-	public void handle(ActionEvent event) {
-			Platform.exit();
-	        System.exit(0);
-	}
-	
-
 }
--- src/View/MainMenu/MainMenu.java
+++ src/View/MainMenu/MainMenu.java
@@ -1,5 +1,49 @@
 package View.MainMenu;
 
-public class MainMenu {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application{
+	
+	Button buttonStart, buttonExit, b3;
+	Scene menu, gameWindow;
+	
+	public void start(Stage stage){
+		
+			stage.setTitle("FantasyTD 1.0");
+			
+			buttonStart = new Button();
+			buttonStart.setText("Start game");
+			buttonStart.setOnAction(e -> stage.setScene(gameWindow));
+			
+			buttonExit = new Button();
+			buttonExit.setText("Exit");
+			buttonExit.setOnAction(e -> stage.close());
+			
+			b3 = new Button();
+			b3.setText("Back to main menu");
+			b3.setOnAction(e -> stage.setScene(menu));
+			
+			VBox layout = new VBox(10);
+			layout.setAlignment(Pos.CENTER);
+			layout.getChildren().add(buttonStart);
+			layout.getChildren().add(buttonExit);
+			
+			VBox layout2 = new VBox();
+			layout2.setAlignment(Pos.CENTER);
+			layout2.getChildren().add(b3);
+			
+			
+			menu= new Scene(layout, 300, 300);
+			stage.setScene(menu);
+			stage.show();
+			
+			gameWindow= new Scene(layout2, 300, 300);
+			
+		}
 
 }
