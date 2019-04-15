f5408d2f84791625763935e588da4a0bb20d1e08
Ismam Abu - ismam.abu@studio.unibo.it <ismam.abu@studio.unibo.it>
2019-04-15 14:38:43 +0200

    Added main

---
+++ src/Main/FantasyTD.java
@@ -0,0 +1,12 @@
+package Main;
+
+import View.MainMenu.MainMenu;
+import javafx.application.Application;
+
+public class FantasyTD{
+	
+	public static void main(String[] args){
+		Application.launch(MainMenu.class);
+		System.out.println("printline di debug");
+	}
+}
