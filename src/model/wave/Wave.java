e9f4a61c1e95b8120361b0a90bca69725d7966e4
Vlad <vlad.mattiussi@studio.unibo.it>
2019-04-24 18:28:45 +0200

    checkstyle

--- src/model/wave/Wave.java
+++ src/model/wave/Wave.java
@@ -2,16 +2,37 @@ package model.wave;
 
 import model.enemy.Enemy;
 import model.enemy.EnemyType;
+/**
+ * 
+ * Interface for waves containing enemies.
+ *
+ */
 public interface Wave {
-
-	int getWave();
-	
-	void populate(int quantity, EnemyType type);
-	
-	Enemy spawn();
-	
-	Wave nextWave();
-
-	boolean hasEnemies();
+/**
+ * 
+ * @return wave number.
+ */
+    int getWave();
+    /**
+     * Method to insert enemies in a wave.
+     * @param quantity quantity
+     * @param type type
+     */
+    void populate(int quantity, EnemyType type);
+    /**
+     * Method to spawn enemies.
+     * @return enemy
+     */
+    Enemy spawn();
+    /**
+     * Method to get next wave.
+     * @return wave
+     */
+    Wave nextWave();
+/**
+ * 
+ * @return true if wave has enemies.
+ */
+    boolean hasEnemies();
 
 }
